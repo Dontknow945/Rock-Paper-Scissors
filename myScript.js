1e170e087681a38818275d9fcbabc4776e06b372
dontknow945 <a0978878897@gmail.com>
2021-12-03 15:17:38 +0800

    add playRound function

--- myScript.js
+++ myScript.js
@@ -3,12 +3,48 @@ function computerPlay() {
     
     switch (computer) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper";
+            return "paper";
         case 3:
-            return "Scissors";
+            return "scissors";
         default:
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "A tie";
+    }
+    
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "paper") {
+                return "You Lose! Paper beats Rock";
+            } else {
+                return "You Win!";
+            }
+        case "paper":
+            if (computerSelection === "scissors") {
+                return "You Lose! Scissors beats Paper";
+            } else {
+                return "You Win!";
+            }
+        case "scissors":
+            if (computerSelection === "rock") {
+                return "You Lose! Rock beats Scissors";
+            } else {
+                return "You Win!";
+            }
+        default:
+            break;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
